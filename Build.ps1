f4ba5e05477995d0d2570d6a14280c0f735b2448
Lukáš Běhal <luk355@gmail.com>
2018-02-13 21:16:26 +1300

    fixing broken build

--- Build.ps1
+++ Build.ps1
@@ -53,12 +53,11 @@ if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
 EnsurePsbuildInstalled
 
 exec { & dotnet restore }
-
-#Invoke-MSBuild
+exec { & dotnet build }
 
 $revision = @{ $true = $env:APPVEYOR_BUILD_NUMBER; $false = 1 }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $revision = "{0:D4}" -f [convert]::ToInt32($revision, 10)
 
 #exec { & dotnet test .\test\Serilog.Sinks.NUnit.Tests -c Release }
 
-exec { & dotnet pack .\src\Serilog.Sinks.NUnit -c Release -o .\artifacts --version-suffix=$revision } 
+exec { & dotnet pack .\src\Serilog.Sinks.NUnit -c Release -o .\artifacts --version-suffix=$revision /p:NoPackageAnalysis=true} 
